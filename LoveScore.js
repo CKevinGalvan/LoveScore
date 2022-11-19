b01ba4434b6e6ee7f260df9a5b00e87b04697a14
Carl Kevin Galvan <99018607+CKevinGalvan@users.noreply.github.com>
2022-11-18 16:28:35 -0800

    Create LoveScore.js

---
+++ LoveScore.js
@@ -0,0 +1,12 @@
+prompt("What's your name? ")
+prompt("What's his/her name? ")
+
+var loveScore = Math.floor(Math.random()*100);
+if(loveScore === 100)
+{
+    console.log("Both of you got 100% of love score!!");
+}
+else
+{
+    console.log("Both of you got "+loveScore+"% love score!");
+}
